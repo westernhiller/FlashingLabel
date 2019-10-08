465aaaf400ebc7d78b2926d33f4b08c9fa419a60
westernhiller <zhangwayne@hotmail.com>
2019-10-08 09:22:11 +0800

    initial creating project

---
+++ flashinglabel.cpp
@@ -0,0 +1,53 @@
+﻿#include "flashinglabel.h"
+#include <QPainter>
+
+FlashingLabel::FlashingLabel(QWidget *parent)
+    : QLabel(parent)
+{
+    m_pTimer = new QTimer();
+    connect(m_pTimer, SIGNAL(timeout()), this, SLOT(onTimer()));
+//    m_pTimer->start(500);
+
+    m_color = Qt::blue;
+}
+
+void FlashingLabel::startFlashing()
+{
+    m_pTimer->start(500);
+}
+
+void FlashingLabel::stopFlashing()
+{
+    m_pTimer->stop();
+}
+
+void FlashingLabel::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHints(QPainter::Antialiasing);
+
+    painter.setBrush(QBrush(m_color,Qt::SolidPattern));
+    QRadialGradient radialGradient(200,200,200,300,300); //辐射渐变 中心点 半径 焦点
+
+    radialGradient.setColorAt(0, Qt::yellow);
+    radialGradient.setColorAt(1, m_color);
+    painter.setBrush(radialGradient);
+
+    painter.drawRect(geometry());
+
+    QLabel::paintEvent(event);
+}
+
+void FlashingLabel::onTimer()
+{
+    static bool bColorBlue = false;
+
+    bColorBlue = !bColorBlue;
+
+    if(bColorBlue)
+        m_color = Qt::blue;
+    else {
+        m_color = Qt::red;
+    }
+    update();
+}
---
+++ flashinglabel.h
@@ -0,0 +1,30 @@
+﻿#ifndef FLASHINGLABEL_H
+#define FLASHINGLABEL_H
+
+#include <QLabel>
+#include <QWidget>
+#include <QTimer>
+#include <QColor>
+
+class FlashingLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit FlashingLabel(QWidget *parent = nullptr);
+
+    void startFlashing();
+    void stopFlashing();
+signals:
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+private:
+    QTimer* m_pTimer;
+    QColor m_color;
+
+public slots:
+    void onTimer();
+
+};
+
+#endif // FLASHINGLABEL_H
